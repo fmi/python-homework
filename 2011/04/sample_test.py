e23a0dff9e136c455e985d00f22473357cc9ef2c
Stefan Kanev <stefan.kanev@gmail.com>
2011-04-28 19:39:06 +0300

    Assignment №4

---
+++ 2011/04/sample_test.py
@@ -0,0 +1,18 @@
+import unittest
+from predicates import gt, lt, pred, for_any, for_all, present, eq, oftype
+
+class PredicatesTest(unittest.TestCase):
+    def test_simple_gt(self):
+        self.assertTrue(gt(2)(4))
+
+    def test_combining_gt_and_lt(self):
+        self.assertTrue((gt(2) & lt(4))(3))
+
+    def test_combining_lt_and_gt(self):
+        self.assertTrue((lt(4) & gt(2))(3))
+
+    def test_pred(self):
+        self.assertTrue(pred(lambda x: x > 2)(4))
+
+if __name__ == '__main__':
+    unittest.main()
