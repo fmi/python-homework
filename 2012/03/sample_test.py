f33c46badd9da090fa1cfcdc75d354ebc994e08c
Kiril Vladimiroff <v.kiril@gmail.com>
2012-04-10 17:03:18 +0300

    Fix tests

--- 2012/03/sample_test.py
+++ 2012/03/sample_test.py
@@ -9,16 +9,17 @@ class BiDictTestCase(unittest.TestCase):
         self.assertEqual(self.person['name'], 'Кънчо')
 
     def test_inverse(self):
-        self.person.iverse()
+        self.person.invert()
         self.assertIn('Кънчо', self.person.keys())
 
     def test_invalid_value(self):
-        self.assertRaises(TypeError, self.person.update({'sports': ['boxing',]}))
+        self.assertRaises(TypeError, self.person.update, {'sports': ['boxing',]})
 
     def test_has_dict_attrs(self):
-        self.assertIn('keys', self.person)
-        self.assertIn('pop', self.person)
-        self.assertIn('copy', self.person)
+        self.assertIn('keys', dir(self.person))
+        self.assertIn('pop', dir(self.person))
+        self.assertIn('copy', dir(self.person))
+
 
 if __name__ == '__main__':
     unittest.main()
