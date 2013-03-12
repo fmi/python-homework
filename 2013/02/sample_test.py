d6c95116a477cf6bb0ca2d0b875abe1d12453e34
Georgy Angelov <georgyangelov@gmail.com>
2013-03-12 22:14:35 +0200

    Made code PEP8 compliant

--- 2013/02/sample_test.py
+++ 2013/02/sample_test.py
@@ -1,20 +1,23 @@
 import unittest
 import solution
 
+
 class SecondHomeworkSimpleTests(unittest.TestCase):
 
     def test_groupby_simple_types(self):
-        expected = {'even':[2, 8, 10, 12], 'odd':[1, 3, 5, 9]}
-        actual = solution.groupby(lambda x: 'odd' if x%2 else 'even', [1, 2, 3, 5, 8, 9, 10, 12])
+        expected = {'even': [2, 8, 10, 12], 'odd': [1, 3, 5, 9]}
+        actual = solution.groupby(lambda x: 'odd' if x % 2 else 'even',
+                                  [1, 2, 3, 5, 8, 9, 10, 12])
         self.assertEqual(expected, actual)
 
     def test_iterate_start_with_identity_function(self):
-        bracketisers = solution.iterate(lambda x: '(' + x + ')') # there's no such word, really
+        # there's no such word, really
+        bracketisers = solution.iterate(lambda x: '(' + x + ')')
         no_brackets = next(bracketisers)
         self.assertEqual('hello world', no_brackets('hello world'))
 
     def test_iterate_ordered_calls(self):
-        powers_of_two = solution.iterate(lambda x: x*2)
+        powers_of_two = solution.iterate(lambda x: x * 2)
         f = next(powers_of_two)
         self.assertEqual(1 * 'eggs', f('eggs'))
         f = next(powers_of_two)
@@ -32,13 +35,13 @@ class SecondHomeworkSimpleTests(unittest.TestCase):
         self.assertEqual(expected, list(actual))
 
     def test_cache_call_is_cached(self):
-
         call_count = 0
+
         def double(x):
             nonlocal call_count
             call_count += 1
             return 2 * x
-        
+
         cached_double = solution.cache(double, 10)
         self.assertEqual(256, cached_double(128))
         self.assertEqual(256, cached_double(128))
