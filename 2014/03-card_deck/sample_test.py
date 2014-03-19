043e09fa3d85029bcd498dd4d46b0a5a46ab3699
Kiril Vladimirov <kiril@vladimiroff.org>
2014-03-19 23:45:06 +0200

    Add 3rd homework

---
+++ 2014/03-card_deck/sample_test.py
@@ -0,0 +1,67 @@
+import unittest
+import random
+
+from solution import Card, RANKS, SUITS, CardCollection
+
+
+def random_card():
+    return Card(random.choice(list(RANKS.values())),
+                random.choice(list(SUITS.values())))
+
+
+class CardTest(unittest.TestCase):
+    def test_card_instance(self):
+        aos = Card(RANKS["Ace"], SUITS["Spades"])
+        self.assertIsInstance(aos.rank, RANKS["Ace"])
+        self.assertIsInstance(aos.suit, SUITS["Spades"])
+
+    def test_card_equals(self):
+        aos1 = Card(RANKS["Ace"], SUITS["Spades"])
+        aos2 = Card(RANKS["Ace"], SUITS["Spades"])
+        self.assertEqual(aos1, aos2)
+
+    def test_suit_rank_equals(self):
+        aos = Card(RANKS["Ace"], SUITS["Spades"])
+        self.assertEqual(aos.rank, RANKS["Ace"]())
+        self.assertEqual(aos.suit, SUITS["Spades"]())
+
+    def test_to_string(self):
+        aos = Card(RANKS["Ace"], SUITS["Spades"])
+        self.assertEqual(str(aos), "Ace of Spades")
+
+
+class CardCollectionTest(unittest.TestCase):
+    def setUp(self):
+        self.deck = [Card(rank, suit) for rank in RANKS.values()
+                     for suit in SUITS.values()]
+
+    def test_standard_deck(self):
+        deck = CardCollection(self.deck)
+        self.assertEqual(len(deck), 52)
+
+    def test_deck_add(self):
+        deck = CardCollection()
+        card1, card2 = random_card(), random_card()
+
+        deck.add(card1)
+        self.assertEqual(deck[0], card1)
+
+        deck.add(card2)
+        self.assertEqual(deck[1], card2)
+
+        self.assertEqual(len(deck), 2)
+
+    def test_deck_draw(self):
+        deck = CardCollection(self.deck)
+        card = deck.top_card()
+
+        self.assertEqual(card, deck.draw_from_top())
+        self.assertEqual(len(deck), 51)
+
+    def test_deck_iteration(self):
+        deck = CardCollection(self.deck)
+        for card in deck:
+            self.assertIsInstance(card, Card)
+
+if __name__ == '__main__':
+    unittest.main()
